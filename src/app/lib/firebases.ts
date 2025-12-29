44c0e6bbe6698aa8f3b0f6d7f3b0ecf6ca28137a
vivek07777777 <abhishekkumarsingh841441@gmail.com>
2025-12-29 17:52:50 +0530

    make changes in /lib/firebase.

--- src/app/lib/firebases.ts
+++ src/app/lib/firebases.ts
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBGxUjfEPFYaCLK6n0FTLFgn9dHvZP1p2Q",
-  authDomain: "our-crazy-code.firebaseapp.com",
-  databaseURL: "https://our-crazy-code-default-rtdb.firebaseio.com",
-  projectId: "our-crazy-code",
-  storageBucket: "our-crazy-code.firebasestorage.app",
-  messagingSenderId: "628267915326",
-  appId: "1:628267915326:web:908ef4d235fd573a40d96f",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 // Initialize Firebase
